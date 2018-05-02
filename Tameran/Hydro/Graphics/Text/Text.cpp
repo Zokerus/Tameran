db6ce674f3d9eca16f95742c642c933bb434dc3d
Tobias Praß <zokerus@hotmail.de>
2018-05-02 21:32:37 +0200

    Renamed variables and deleted Initialize, Shutdown methods and added some properties to Text Class

--- Tameran/Hydro/Graphics/Text/Text.cpp
+++ Tameran/Hydro/Graphics/Text/Text.cpp
@@ -1,58 +1,53 @@
 #include "Text.h"
+#include <exception>
 
-Hydro::Text::Text(int screenWidth, int screenHeight, int maxLength)
-	: m_vertexBuffer(nullptr), m_indexBuffer(nullptr), m_vertexBuffer2(nullptr), m_indexBuffer2(nullptr),
-	m_pixelColor(), m_pos(), m_screenWidth(screenWidth), m_screenHeight(screenHeight), m_maxLength(maxLength),
-	m_vertexCount(0), m_indexCount(0), m_shadow(false), m_ready(false)
-{
-}
-
-Hydro::Text::~Text()
-{
-	if (m_ready)
-	{
-		Shutdown();
-	}
-}
-
-bool Hydro::Text::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, bool shadow, Font* font, const char* text, DirectX::XMINT2 pos, DirectX::XMVECTORF32 color)
+Hydro::Text::Text(Direct3D* direct3D, Font* inFont, int ScreenWidth, int ScreenHeight, int MaxLength, bool Shadow, const std::string* Text, DirectX::XMINT2 Pos, DirectX::XMVECTORF32 Color)
+	: vertexBuffer(nullptr), indexBuffer(nullptr), vertexBuffer2(nullptr), indexBuffer2(nullptr),
+	pixelColor(Color), pos(Pos), screenWidth(ScreenWidth), screenHeight(ScreenHeight), maxLength(MaxLength),
+	vertexCount(0), indexCount(0), shadow(Shadow), update(false), font(inFont), text(*Text)
 {
 	Font::VertexType* vertices;
 	unsigned long* indices;
 	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
 	D3D11_SUBRESOURCE_DATA vertexData, indexData;
 	HRESULT result;
-	int i, index;
+	int i, index, numLetters;
 
-	//Store if this sentence is shadowed or not
-	m_shadow = shadow;
+	// Get the number of letters in the sentence.
+	numLetters = (int)Text->length();
+
+	// Check for possible buffer overflow.
+	if (numLetters > maxLength)
+	{
+		throw std::exception("Text length is to long");
+	}
 
 	//Set the vertex and index count
-	m_vertexCount = m_maxLength * 4;
-	m_indexCount = m_maxLength * 6;
+	vertexCount = maxLength * 4;
+	indexCount = maxLength * 6;
 
 	// Create the vertex array.
-	vertices = new Font::VertexType[m_vertexCount];
+	vertices = new Font::VertexType[vertexCount];
 	if (!vertices)
 	{
-		return false;
+		throw std::exception("Failed to create vertex array in text class");
 	}
 
 	// Create the index array.
-	indices = new unsigned long[m_indexCount];
+	indices = new unsigned long[indexCount];
 	if (!indices)
 	{
-		return false;
+		throw std::exception("Failed to create index array in text class");
 	}
 
 	// Initialize vertex array to zeros at first.
-	memset(vertices, 0, (sizeof(Font::VertexType) * m_vertexCount));
+	memset(vertices, 0, (sizeof(Font::VertexType) * vertexCount));
 
 	//Initialize the index of the index array
 	index = 0;
 
 	// Initialize the index array.
-	for (i = 0; i<m_maxLength; i++)
+	for (i = 0; i<maxLength; i++)
 	{
 		indices[index++] = i * 4;
 		indices[index++] = i * 4 + 1;
@@ -65,7 +60,7 @@ bool Hydro::Text::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceCo
 
 	// Set up the description of the dynamic vertex buffer.
 	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-	vertexBufferDesc.ByteWidth = sizeof(Font::VertexType) * m_vertexCount;
+	vertexBufferDesc.ByteWidth = sizeof(Font::VertexType) * vertexCount;
 	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	vertexBufferDesc.MiscFlags = 0;
@@ -77,15 +72,15 @@ bool Hydro::Text::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceCo
 	vertexData.SysMemSlicePitch = 0;
 
 	// Create the vertex buffer.
-	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	result = direct3D->GetDevice()->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer);
 	if (FAILED(result))
 	{
-		return false;
+		throw std::exception("Failed to create vertex buffer in text class");
 	}
 
 	// Set up the description of the static index buffer.
 	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * indexCount;
 	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
 	indexBufferDesc.CPUAccessFlags = 0;
 	indexBufferDesc.MiscFlags = 0;
@@ -97,82 +92,78 @@ bool Hydro::Text::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceCo
 	indexData.SysMemSlicePitch = 0;
 
 	// Create the index buffer.
-	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer);
+	result = direct3D->GetDevice()->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer);
 	if (FAILED(result))
 	{
-		return false;
+		throw std::exception("Failed to create index buffer in text class");
 	}
 
 	//If shadowed create the second vertex and index buffer
-	if (m_shadow)
+	if (shadow)
 	{
-		result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer2);
+		result = direct3D->GetDevice()->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer2);
 		if (FAILED(result))
 		{
-			return false;
+			throw std::exception("Failed to create vertex buffer of shadow in text class");
 		}
 
-		result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer2);
+		result = direct3D->GetDevice()->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer2);
 		if (FAILED(result))
 		{
-			return false;
+			throw std::exception("Failed to create index buffer of shadow in text class");
 		}
 	}
 
 	// Release the vertex array as it is no longer needed.
 	delete[] vertices;
-	vertices = 0;
+	vertices = nullptr;
 
 	// Release the index array as it is no longer needed.
 	delete[] indices;
-	indices = 0;
+	indices = nullptr;
 
 	//Now add the text data to the sentece buffers
-	result = UpdateSentence(deviceContext, font, text, pos, color);
+	result = UpdateSentence(direct3D->GetDeviceContext());
 	if (!result)
 	{
-		return false;
+		throw std::exception("Failed to create Text object");
 	}
 
-	m_ready = true;
-
-	return true;
 }
 
-void Hydro::Text::Shutdown()
+Hydro::Text::~Text()
 {
+	font = nullptr;
 	// Release the vertex and index buffers.
-	if (m_indexBuffer2)
+	if (indexBuffer2)
 	{
-		m_indexBuffer2->Release();
-		m_indexBuffer2 = 0;
+		indexBuffer2->Release();
+		indexBuffer2 = nullptr;
 	}
 
-	if (m_vertexBuffer2)
+	if (vertexBuffer2)
 	{
-		m_vertexBuffer2->Release();
-		m_vertexBuffer2 = 0;
+		vertexBuffer2->Release();
+		vertexBuffer2 = nullptr;
 	}
 
-	if (m_indexBuffer)
+	if (indexBuffer)
 	{
-		m_indexBuffer->Release();
-		m_indexBuffer = 0;
+		indexBuffer->Release();
+		indexBuffer = nullptr;
 	}
 
-	if (m_vertexBuffer)
+	if (vertexBuffer)
 	{
-		m_vertexBuffer->Release();
-		m_vertexBuffer = 0;
+		vertexBuffer->Release();
+		vertexBuffer = nullptr;
 	}
-
-	m_ready = false;
 }
 
-bool Hydro::Text::Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView* fontTexture)
+bool Hydro::Text::Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix)
 {
 	//Draw the sentence
-	if (!RenderSentence(deviceContext, shaderManager, worldMatrix, viewMatrix, orthoMatrix, fontTexture, m_pixelColor))
+	if (!RenderSentence(deviceContext, shaderManager, worldMatrix, viewMatrix, orthoMatrix, pixelColor))
 	{
 		return false;
 	}
@@ -180,59 +171,67 @@ bool Hydro::Text::Render(ID3D11DeviceContext* deviceContext, ShaderManager* shad
 	return true;
 }
 
-bool Hydro::Text::Render(ID3D11DeviceContext * deviceContext, ShaderManager * shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView * fontTexture, DirectX::XMVECTORF32 foreColor)
+bool Hydro::Text::Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, DirectX::XMVECTORF32 foreColor)
 {
 	//Draw the sentence
-	if(!RenderSentence(deviceContext, shaderManager, worldMatrix, viewMatrix, orthoMatrix, fontTexture, foreColor))
+	if(!RenderSentence(deviceContext, shaderManager, worldMatrix, viewMatrix, orthoMatrix, foreColor))
 	{
 		return false;
 	}
 	return true;
 }
 
-bool Hydro::Text::UpdateSentence(ID3D11DeviceContext * deviceContext, Font * font, const char * text, DirectX::XMINT2 pos, DirectX::XMVECTORF32 color)
+void Hydro::Text::SetText(const std::string* Text)
 {
-	int numLetters;
+	text = *Text;
+	update = true;
+}
+
+void Hydro::Text::SetPosition(DirectX::XMINT2 Pos)
+{
+	pos = Pos;
+	update = true;
+}
+
+void Hydro::Text::SetFont(Font* inFont)
+{
+	font = inFont;
+	update = true;
+}
+
+void Hydro::Text::SetColor(DirectX::XMVECTORF32 Color)
+{
+	pixelColor = Color;
+	update = true;
+}
+
+bool Hydro::Text::UpdateSentence(ID3D11DeviceContext* deviceContext)
+{
 	Font::VertexType* vertices;
 	float drawX, drawY;
 	HRESULT result;
 	D3D11_MAPPED_SUBRESOURCE mappedResource;
 	Font::VertexType* verticesPtr;
 
-	//Store the position of the sentence
-	m_pos = pos;
-
-	// Store the color of the sentence.
-	m_pixelColor = color;
-
-	// Get the number of letters in the sentence.
-	numLetters = (int)strlen(text);
-
-	// Check for possible buffer overflow.
-	if (numLetters > m_maxLength)
-	{
-		return false;
-	}
-
 	// Create the vertex array.
-	vertices = new Font::VertexType[m_vertexCount];
+	vertices = new Font::VertexType[vertexCount];
 	if (!vertices)
 	{
 		return false;
 	}
 
 	// Initialize vertex array to zeros at first.
-	memset(vertices, 0, (sizeof(Font::VertexType) * m_vertexCount));
+	memset(vertices, 0, (sizeof(Font::VertexType) * vertexCount));
 
 	// Calculate the X and Y pixel position on the screen to start drawing to.
-	drawX = (float)(((m_screenWidth / 2) * -1) + pos.x);
-	drawY = (float)((m_screenHeight / 2) - pos.y);
+	drawX = (float)(((screenWidth / 2) * -1) + pos.x);
+	drawY = (float)((screenHeight / 2) - pos.y);
 
 	// Use the font class to build the vertex array from the sentence text and sentence draw location.
-	font->BuildVertexArray((void*)vertices, text, drawX, drawY);
+	font->BuildVertexArray((void*)vertices, &text, drawX, drawY);
 
 	// Lock the vertex buffer so it can be written to.
-	result = deviceContext->Map(m_vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	result = deviceContext->Map(vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
 	if (FAILED(result))
 	{
 		return false;
@@ -242,27 +241,30 @@ bool Hydro::Text::UpdateSentence(ID3D11DeviceContext * deviceContext, Font * fon
 	verticesPtr = (Font::VertexType*)mappedResource.pData;
 
 	// Copy the data into the vertex buffer.
-	memcpy(verticesPtr, (void*)vertices, (sizeof(Font::VertexType) * m_vertexCount));
+	memcpy(verticesPtr, (void*)vertices, (sizeof(Font::VertexType) * vertexCount));
 
 	// Unlock the vertex buffer.
-	deviceContext->Unmap(m_vertexBuffer, 0);
+	deviceContext->Unmap(vertexBuffer, 0);
 
 	//If shadowed then do the same for the second vertex buffer but offset by two pixels on both axis
 	//TODO shadow für den text einfügen
-	if (m_shadow)
+	if (shadow)
 	{
-		memset(vertices, 0, (sizeof(Font::VertexType) * m_vertexCount));
+		memset(vertices, 0, (sizeof(Font::VertexType) * vertexCount));
 
-		drawX = (float)((((m_screenWidth / 2) * -1) + pos.x) + 2);
-		drawY = (float)(((m_screenHeight / 2) - pos.y) - 2);
-		font->BuildVertexArray((void*)vertices, text, drawX, drawY);
+		drawX = (float)((((screenWidth / 2) * -1) + pos.x) + 2);
+		drawY = (float)(((screenHeight / 2) - pos.y) - 2);
+		font->BuildVertexArray((void*)vertices, &text, drawX, drawY);
 
-		result = deviceContext->Map(m_vertexBuffer2, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+		result = deviceContext->Map(vertexBuffer2, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
 		if (FAILED(result))
+		{
 			return false;
+		}
+		
 		verticesPtr = (Font::VertexType*)mappedResource.pData;
-		memcpy(verticesPtr, (void*)vertices, (sizeof(Font::VertexType) * m_vertexCount));
-		deviceContext->Unmap(m_vertexBuffer2, 0);
+		memcpy(verticesPtr, (void*)vertices, (sizeof(Font::VertexType) * vertexCount));
+		deviceContext->Unmap(vertexBuffer2, 0);
 	}
 
 	// Release the vertex array as it is no longer needed.
@@ -272,45 +274,68 @@ bool Hydro::Text::UpdateSentence(ID3D11DeviceContext * deviceContext, Font * fon
 	return true;
 }
 
-bool Hydro::Text::RenderSentence(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView * fontTexture, DirectX::XMVECTORF32 foreColor)
+const Hydro::Font* Hydro::Text::GetFont() const
+{
+	return font;
+}
+
+const std::string* Hydro::Text::GetText() const
+{
+	return &text;
+}
+
+bool Hydro::Text::RenderSentence(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, DirectX::XMVECTORF32 foreColor)
 {
 	unsigned int stride, offset;
 	DirectX::XMVECTORF32 shadowColor;
 	bool result;
 
+	//Update text object if needed
+	if (update)
+	{
+		if (!UpdateSentence(deviceContext))
+		{
+			return false;
+		}
+		update = false;
+	}
 
 	// Set vertex buffer stride and offset.
 	stride = sizeof(Font::VertexType);
 	offset = 0;
 
 	//If shadowed then render the shadow text first
-	if (m_shadow)
+	if (shadow)
 	{
 		shadowColor = DirectX::Colors::Black;
 
-		deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer2, &stride, &offset);
-		deviceContext->IASetIndexBuffer(m_indexBuffer2, DXGI_FORMAT_R32_UINT, 0);
+		deviceContext->IASetVertexBuffers(0, 1, &vertexBuffer2, &stride, &offset);
+		deviceContext->IASetIndexBuffer(indexBuffer2, DXGI_FORMAT_R32_UINT, 0);
 		deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-		result = shaderManager->RenderFontShader(deviceContext, m_indexCount, worldMatrix, viewMatrix, orthoMatrix, fontTexture, shadowColor);
+		result = shaderManager->RenderFontShader(deviceContext, indexCount, worldMatrix, viewMatrix, orthoMatrix, font->GetTexture(), shadowColor);
 		if (!result)
+		{
 			return false;
+		}
 	}
 
 	//Render the text buffer
 	// Set the vertex buffer to active in the input assembler so it can be rendered.
-	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+	deviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
 
 	// Set the index buffer to active in the input assembler so it can be rendered.
-	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+	deviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
 
 	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
 	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	// Render the text using the font shader.
-	result = shaderManager->RenderFontShader(deviceContext, m_indexCount, worldMatrix, viewMatrix, orthoMatrix, fontTexture, foreColor);
+	result = shaderManager->RenderFontShader(deviceContext, indexCount, worldMatrix, viewMatrix, orthoMatrix, font->GetTexture(), foreColor);
 	if (!result)
+	{
 		false;
+	}
 
 	return true;
 }
--- Tameran/Hydro/Graphics/Text/Text.h
+++ Tameran/Hydro/Graphics/Text/Text.h
@@ -3,6 +3,7 @@
 
 //Includes
 #include <DirectXColors.h>
+#include <string>
 
 //My Includes
 #include "Font.h"
@@ -16,24 +17,31 @@ namespace Hydro
 	class Text
 	{
 	public:
-		Text(int screenWidth, int screenHeight, int maxLength);
+		Text(Direct3D* direct3D, Font* inFont, int ScreenWidth, int ScreenHeight, int MaxLength, bool Shadow, const std::string* Text, DirectX::XMINT2 Pos, DirectX::XMVECTORF32 Color);
 		~Text();
 
-		bool Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, bool shadow, Font* font, const char* text, DirectX::XMINT2 pos, DirectX::XMVECTORF32 color);
-		void Shutdown();
-		bool Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView* fontTexture);
-		bool Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView* fontTexture, DirectX::XMVECTORF32 foreColor);
+		bool Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix);
+		bool Render(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, DirectX::XMVECTORF32 foreColor);
 	
-		bool UpdateSentence(ID3D11DeviceContext* deviceContext, Font* font, const char* text, DirectX::XMINT2 pos, DirectX::XMVECTORF32 color);
+		void SetText(const std::string* Text);
+		void SetPosition(DirectX::XMINT2 Pos);
+		void SetFont(Font* inFont);
+		void SetColor(DirectX::XMVECTORF32 Color);
+
+		const Font* GetFont() const;
+		const std::string* GetText() const;
 	private:
-		bool RenderSentence(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, ID3D11ShaderResourceView* fontTexture, DirectX::XMVECTORF32 foreColor);
+		bool UpdateSentence(ID3D11DeviceContext* deviceContext);
+		bool RenderSentence(ID3D11DeviceContext* deviceContext, ShaderManager* shaderManager, DirectX::XMMATRIX worldMatrix, DirectX::XMMATRIX viewMatrix, DirectX::XMMATRIX orthoMatrix, DirectX::XMVECTORF32 foreColor);
 	
 	private:
-		ID3D11Buffer *m_vertexBuffer, *m_indexBuffer, *m_vertexBuffer2, *m_indexBuffer2;
-		DirectX::XMVECTORF32 m_pixelColor;
-		DirectX::XMINT2 m_pos;
-		int m_screenWidth, m_screenHeight, m_maxLength, m_vertexCount, m_indexCount;
-		bool m_shadow, m_ready;
+		ID3D11Buffer *vertexBuffer, *indexBuffer, *vertexBuffer2, *indexBuffer2;
+		DirectX::XMVECTORF32 pixelColor;
+		DirectX::XMINT2 pos;
+		int screenWidth, screenHeight, maxLength, vertexCount, indexCount;
+		bool shadow, update;
+		Font* font;
+		std::string text;
 	};
 }
 
